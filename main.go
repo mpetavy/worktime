5d4d604999a3297d5140e8a0406857f59fde0ba9
mpetavy <mpetavy>
2021-03-14 22:08:39 +0100

    app.resources with embed.FS

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ type Day struct {
 }
 
 func init() {
-	common.Init(true, "1.0.27", "", "2017", "tracks your working times", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, run, time.Duration(60)*time.Second)
+	common.Init(true, "1.0.27", "", "2017", "tracks your working times", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, nil, run, time.Duration(60)*time.Second)
 
 	minutes = flag.Bool("m", false, "show durations in minutes")
 
